67f38ecca5cf5a67e904105bf33090e975888b63
somkiat lee <somkarun@gmail.com>
2021-08-15 08:57:12 +0700

    update index1

--- app/routes.py
+++ app/routes.py
@@ -45,7 +45,7 @@ def preprocess(img):
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index3.html')
+    return render_template('index1.html')
 
 @app.route('/predict', methods=["POST", "OPTIONS"])
 def predict():
